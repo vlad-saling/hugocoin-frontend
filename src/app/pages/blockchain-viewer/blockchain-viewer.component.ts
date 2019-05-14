c221bbc4e8c4a9b42ddb923beacc3a3ec76995f3
hugo <hugo@oc8807034253.ibm.com>
2019-05-14 12:48:50 +0200

    x

--- src/app/pages/blockchain-viewer/blockchain-viewer.component.ts
+++ src/app/pages/blockchain-viewer/blockchain-viewer.component.ts
@@ -8,12 +8,16 @@ import { BlockchainService } from 'src/app/services/blockchain.service';
 })
 export class BlockchainViewerComponent implements OnInit {
   public blocks = [];
+  public selectedBlock = null;
 
   constructor(private blockchainService: BlockchainService) {
     this.blocks = blockchainService.getBlocks();
+    this.selectedBlock = this.blocks[0];
   }
 
   ngOnInit() {
   }
-
+  showTransactions(block) {
+    this.selectedBlock = block;
+  }
 }
